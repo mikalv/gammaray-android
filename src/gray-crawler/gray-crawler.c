eaf45313d6398b6df0318d9764a6b8aca647f5fb
Brandon Amos <bamos@cmu.edu>
2014-09-27 14:59:11 -0400

    Clean up imports for #9.

--- src/gray-crawler/gray-crawler.c
+++ src/gray-crawler/gray-crawler.c
@@ -36,16 +36,8 @@
 #include "mbr.h"
 #include "ntfs.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netdb.h>
 
 /* supported file system serializers */
 struct gray_fs_crawler crawlers[] = {
